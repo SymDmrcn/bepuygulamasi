bfc3341c856e9ed370ae34ddeae8c399d9ccc91d
SymDmrcn <117096224+SymDmrcn@users.noreply.github.com>
2025-04-13 23:59:02 +0300

    Update streamlit_bep_app.py

--- streamlit_bep_app.py
+++ streamlit_bep_app.py
@@ -2,6 +2,7 @@ import json
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
+from docx import Document  # Word çıktısı için gerekli
 
 firebase_config = st.secrets["firebase_config"]
 cred = credentials.Certificate(dict(firebase_config))
